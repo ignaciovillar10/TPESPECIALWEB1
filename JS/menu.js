536f4a0c343b58be16ffa5bbd24528cc7d25e10a
ignaciovillar10 <ignaciovillaar@gmail.com>
2025-05-09 18:14:23 +0000

    agrego js

---
+++ JS/menu.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+inicializarMenu();
+function inicializarMenu() {
+    const toggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
+  
+    if (toggle && menu) {
+      toggle.addEventListener("click", () => {
+        menu.classList.toggle("mostrar");
+      });
+    }
+  }
+});
---
+++ JS/modooscuro.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    inicializarModoOscuro();
+function inicializarModoOscuro() {
+    const botonModo = document.getElementById("modo-toggle");
+  }
+});
